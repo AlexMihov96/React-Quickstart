8461f3f7ffec2ede8e5e1be7a07088dc4728783c
AlexMihov96 <alexandarmihov1@gmail.com>
2017-11-18 02:27:28 +0200

    Adding All neccessery packages
    Added Routing, Redux, Bootstrap

--- app/src/index.js
+++ app/src/index.js
@@ -2,7 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// const store = createStore(
+//     combineReducers(reducers),
+//     applyMiddleware(thunk)
+// )
+const store = {}
+
+ReactDOM.render((
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>), document.getElementById('root'))
+registerServiceWorker()
